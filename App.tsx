08d179864b97e9a19c62f102e98402853e12d9d2
Jimmy <abc751202@gmail.com>
2019-04-01 16:53:16 +0800

    add webview

--- App.tsx
+++ App.tsx
@@ -1,13 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, {PureComponent} from 'react';
+import {Platform, StyleSheet, WebView} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -17,14 +9,13 @@ const instructions = Platform.select({
 });
 
 type Props = {};
-export default class App extends Component<Props> {
+export default class App extends PureComponent<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <WebView
+        source={{uri: 'http://www.blg5888.com'}}
+        style={{marginTop: 0}}
+      />
     );
   }
 }
