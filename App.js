7f434fdb078a31afc4968b0192310b1d7e433542
Jimmy <abc751202@gmail.com>
2019-04-02 12:52:36 +0800

    handle android back button

--- App.js
+++ App.js
@@ -1,17 +1,44 @@
 import React, {PureComponent} from 'react';
-import {Platform, StyleSheet, View, Text, Dimensions} from 'react-native';
+import {Platform, StyleSheet, View, BackHandler, Dimensions} from 'react-native';
 import { WebView } from "react-native-webview";
 import config from './config';
 
 const window = Dimensions.get('window');
 
 export default class App extends PureComponent {
+  webView = {
+    canGoBack: false,
+    ref: null,
+  }
+
+  onAndroidBackPress = () => {
+    if (this.webView.canGoBack && this.webView.ref) {
+      this.webView.ref.goBack();
+      return true;
+    }
+    return false;
+  }
+
+  componentWillMount() {
+    if (Platform.OS === 'android') {
+      BackHandler.addEventListener('hardwareBackPress', this.onAndroidBackPress);
+    }
+  }
+
+  componentWillUnmount() {
+    if (Platform.OS === 'android') {
+      BackHandler.removeEventListener('hardwareBackPress');
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <WebView
           source={{uri: config.uri}}
           style={styles.webView}
+          ref={(webView) => { this.webView.ref = webView; }}
+          onNavigationStateChange={(navState) => { this.webView.canGoBack = navState.canGoBack; }}
         />
       </View>
     );
