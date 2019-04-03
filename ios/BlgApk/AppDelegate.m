73a385cce233eb21945c0b61fb450830f1857a1e
Jimmy <abc751202@gmail.com>
2019-04-03 11:00:10 +0800

    bundle for ios

--- ios/BlgApk/AppDelegate.m
+++ ios/BlgApk/AppDelegate.m
@@ -33,9 +33,9 @@
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
 #if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
 #else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  return [[NSBundle mainBundle] URLForResource:@"bundle/index.ios" withExtension:@"jsbundle"];
 #endif
 }
 
